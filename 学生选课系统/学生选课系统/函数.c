6ea6a67edd8aeb476b7e00dd889b237fefd278ba
lansengit <t61654@163.com>
2019-06-18 17:48:45 +0800

    终于写完了……

--- 学生选课系统/学生选课系统/函数.c
+++ 学生选课系统/学生选课系统/函数.c
@@ -1,7 +1,7 @@
 #pragma warning(disable:4996);
 typedef struct
 {
-	int course_number;		//课程编号
+	int course_number1;		//课程编号
 	char course_name[128];	//课程名称
 	int course_hours;		//学时
 	int course_people;		//限选人数
@@ -10,10 +10,13 @@ typedef struct
 {
 	int student_number;		//学号
 	char student_name[128]; //姓名
-	int course_number;		//课程编号
+	int course_number2;		//课程编号
 } Student;
-#define N 2	//一共录入的课程数
+#define N 5	//一共录入的课程数
+#define M 14 //选课信息数组长度
 #include<stdio.h>
+#include <string.h> 
+
 
 void fun1(Course * course)		
 {
@@ -22,15 +25,15 @@ void fun1(Course * course)
 	for (int i = 0; i < N; i++)
 	{
 		printf("输入第%d门课程信息:\n", i + 1);
-		scanf("%d%s%d%d", &course[i].course_number, &course[i].course_name, &course[i].course_hours, &course[i].course_people);
+		scanf("%d%s%d%d", &course[i].course_number1, &course[i].course_name, &course[i].course_hours, &course[i].course_people);
 		if (i >= 1)
 		{
 			for (int j = i - 1; j >= 0; j--)
 			{
-				if (course[i].course_number == course[j].course_number)
+				if (course[i].course_number1 == course[j].course_number1)
 				{
 					printf("课程编号重复，重新输入第%d门课程编号：\n", i + 1);
-					scanf("%d", &course[i].course_number);
+					scanf("%d", &course[i].course_number1);
 					j = i;	//从头开始比较
 				}
 			}
@@ -48,3 +51,145 @@ void fun1(Course * course)
 	}
 	
 }
+void fun2(Course * course)
+{
+	printf("显示内容依次为：课程编号、课程名称、学时、限选人数\n");
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d %s %d %d\n", course[i].course_number1, course[i].course_name, course[i].course_hours, course[i].course_people);
+	}
+}
+void fun3(Student * student,Course * course)
+{
+	int sum[N] = { 0 };
+	printf("分别输入学号、姓名、课程编号，用空格隔开，输入完后按回车\n");
+	for (int i = 0; i < M; i++)
+	{
+		printf("输入第%d项选课信息:\n", i + 1);
+		scanf("%d%s%d",&student[i].student_number,&student[i].student_name,&student[i].course_number2 );
+		if (i >= 1)
+		{
+			for (int j = i - 1; j >= 0; j--)
+			{
+				if (student[i].course_number2 == student[j].course_number2&&student[i].student_number == student[j].student_number)
+				{
+					printf("学号和课程编号同时重复，重新输入第%d项选课信息,依次输入学号、姓名、课程编号：\n", i + 1);
+					scanf("%d%s%d", &student[i].student_number, &student[i].student_name, &student[i].course_number2);
+					j = i;	//从头开始比较
+				}
+			}
+		}
+
+		
+		while (1)
+		{
+			int k = -1;
+			do {
+				k++;
+			} while (student[i].course_number2 != course[k].course_number1);
+			sum[k]++;
+			if (sum[k] > course[k].course_people)
+			{
+				printf("编号为%d的课程超过限定人数，更改所选课程编号：\n", course[k].course_number1);
+				scanf("%d", &student[i].course_number2);
+			}
+			else
+				break;
+		}
+
+	}
+	for (int l = 0; l < M; l++)
+		printf("%d %s %d\n", student[l].student_number, student[l].student_name, student[l].course_number2);
+
+}
+void fun4(Course * course)
+{
+	printf("输入要查询的课程名称：\n");
+	char mingzi[128];
+	scanf("%s", mingzi);
+	
+		int temp = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (strstr(course[i].course_name, mingzi) != NULL)
+			{
+				temp++;
+				printf("%d %s %d %d\n", course[i].course_number1, course[i].course_name, course[i].course_hours, course[i].course_people);
+			}
+		}
+		if (temp == 0)
+			printf("无相关课程信息\n");
+}
+void fun5(Student * student)
+{
+	printf("输入学号：\n");
+	int xuehao;
+	scanf("%d", &xuehao);
+	int temp = 0;
+	for (int i = 0; i < M; i++)
+	{
+		if (xuehao==student[i].student_number)
+		{
+			temp++;
+			printf("%d %s %d\n", student[i].student_number, student[i].student_name, student[i].course_number2);
+		}
+	}
+	if (temp == 0)
+		printf("无相关学生信息\n");
+	
+}
+void fun6(Course * course)
+{
+	printf("输入课程编号：\n");
+	int kcbh;
+	scanf("%d",&kcbh);
+	int i;
+	int temp=0;
+	for (i = 0; i < N; i++)
+		if (kcbh == course[i].course_number1)
+			break;
+		else
+			temp++;
+	if (temp == N)		printf("没有该课程\n");
+	printf("将编号为%d的课程的学时、限选人数分别修改为：（空格间隔）\n", course[i].course_number1);
+	scanf("%d %d", &course[i].course_hours, &course[i].course_people);
+}
+void fun7(Student * student)
+{
+	printf("输入学号：\n");
+	int xuehao;
+	scanf("%d", &xuehao);
+	int temp = 0;
+	for (int i = 0; i < M; i++)
+	{
+		if (xuehao == student[i].student_number)
+		{
+			temp++;
+			memset(&student[i],NULL, sizeof(Student));
+		}
+	}
+	if (temp == 0)
+		printf("无相关学生信息\n");
+	for (int i = 0; i < M; i++)
+	{
+		printf("%d %s %d\n", student[i].student_number, student[i].student_name, student[i].course_number2);
+	}
+}
+void fun8(Student * student)
+{
+	printf("输入课程编号：\n");
+	int bianhao;
+	scanf("%d",& bianhao);
+	int i;
+	int temp = 0;
+	for (i = 0; i < M; i++)
+	{
+		if (bianhao == student[i].course_number2)
+			printf("%d %s\n", student[i].student_number, student[i].student_name);
+		else
+			temp++;
+	}
+	if (temp == M)		printf("没有该课程\n");
+	printf("总人数为%d\n", M - temp);
+}
+
--- 学生选课系统/学生选课系统/源.c
+++ 学生选课系统/学生选课系统/源.c
@@ -13,14 +13,20 @@ typedef struct
 	char student_name[129]; //姓名
 	int course_number;		//课程编号
 } Student;
+
+
 #include<stdio.h>
-#define N 2	//一共录入的课程数
-void fun1(Course *);	//1.课程信息的录入和保存
+#define N 5	//一共录入的课程数
+#define M 14 //选课信息数组长度
+
+
+
 
 void main()
 {
 	Course course[N];	//用来存放所有课程信息的结构数组 
-/***********************************************菜单起始********************************************************************/
+	Student student[M];
+//菜单起始
 	while (1)
 	{
 		printf("************************系统菜单功能项****************************\n");
@@ -37,33 +43,31 @@ void main()
 			fun1(course);
 			break;
 		case 2:
-
+			fun2(course);
 			break;
 		case 3:
-
+			fun3(student,course);
 			break;
 		case 4:
-
+			fun4(course);
 			break;
 		case 5:
-
+			fun5(student);
 			break;
 		case 6:
-
+			fun6(course);
 			break;
 		case 7:
-
+			fun7(student);
 			break;
 		case 8:
-
+			fun8(student);
 			break;
 		case 9:
-
-			break;
+			goto lam;
 		default:
 			printf("\n 对不起，您的选择有误，请重新输入！！！\n");
 		}
 	}
-/********************************************菜单结束***********************************************************************/
-
+	lam:;
 }
